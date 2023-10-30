44c15ae5f74b5b9bab1f3cf13bc63760c7e1a086
Amisha Prasad <amishaprasad@As-Computer.local>
2023-10-29 23:55:40 -0700

    histogram

--- asgn2/histogram/histogram.c
+++ asgn2/histogram/histogram.c
@@ -1,55 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_BINS 16
-
-void printHistogram(int bins[], int binSize, int range);
+#define NUMBINS 16
 
 int main() {
-    int bins[NUM_BINS] = {0};
-    int binSize = 1;
-    int range = NUM_BINS;
-    int num;
+  int bins[NUMBINS] = {0};
+  int bin_size = 1;
+  int MAXIMUMVALUE = bin_size * NUMBINS;
+  int input_num;
 
-    while (scanf("%d", &num) != EOF) {
-        if (num < 0) {
-            continue; // Ignore negative numbers.
-        }
-
-        // If the number is outside the range, expand the bins.
-        while (num >= range) {
-            // Compress current bins into the first half.
-            for (int i = 0; i < NUM_BINS / 2; i++) {
-                bins[i] = bins[2 * i] + bins[2 * i + 1];
-            }
-
-            // Clear the second half of the bins.
-            for (int i = NUM_BINS / 2; i < NUM_BINS; i++) {
-                bins[i] = 0;
-            }
-
-            // Double the bin size and range.
-            binSize *= 2;
-            range *= 2;
-        }
-
-        // Increment the appropriate bin.
-        bins[num / binSize]++;
+  while (scanf("%d", &input_num) != EOF) {
+    if (input_num < 0) {
+        continue;
     }
 
-    // Print the histogram.
-    printHistogram(bins, binSize, range);
+    while (input_num >= MAXIMUMVALUE) {
+      bin_size *= 2;
+      MAXIMUMVALUE = bin_size * NUMBINS;
 
-    return 0;
-}
+      for (int i = 0; i < NUMBINS / 2; i++) {
+        bins[i] = bins[2*i] + bins[2*i + 1];
+      }
 
-void printHistogram(int bins[], int binSize, int range) {
-    printf("Histogram (bin size: %d, range: 0 to %d):\n", binSize, range - 1);
-    for (int i = 0; i < NUM_BINS; i++) {
-        printf("%3d - %3d : ", i * binSize, (i + 1) * binSize - 1);
-        for (int j = 0; j < bins[i]; j++) {
-            printf("#");
-        }
-        printf("\n");
+      for (int i = NUMBINS / 2; i < NUMBINS; i++) {
+        bins[i] = 0;
+      }
     }
+
+    int index = input_num / bin_size;
+    bins[index] += 1;
+  }
+
+  for (int i = 0; i < NUMBINS; i++) {
+    printf("[%3d:%3d] ", i*bin_size, (i+1)*bin_size-1);
+    for (int j = 0; j < bins[i]; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  return 0;
 }
