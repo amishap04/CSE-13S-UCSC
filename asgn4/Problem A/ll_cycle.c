acb3ee3a20fb5d039b8f7a7de4d2410a2f209b22
Amisha Prasad <amishaprasad@As-Computer.local>
2023-11-14 15:29:38 -0800

    attempt 1

--- asgn4/Problem A/ll_cycle.c
+++ asgn4/Problem A/ll_cycle.c
@@ -2,5 +2,22 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    if (head == NULL || head->next == NULL) {
+        return 0; // No cycle if the list is empty or has only one node
+    }
+
+    struct node *slow_ptr = head;
+    struct node *fast_ptr = head;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        slow_ptr = slow_ptr->next;          // Move slow pointer by 1 node
+        fast_ptr = fast_ptr->next->next;    // Move fast pointer by 2 nodes
+
+        if (slow_ptr == fast_ptr) {
+            return 1; // Cycle found
+        }
+    }
+
+    return 0; // No cycle found
+}
 }
