d95342501a048b28d82dae64638c9edd9c3e5fde
Amisha Prasad <amishaprasad@As-Computer.local>
2023-11-15 23:32:30 -0800

    please last time

--- asgn4/Problem B/growing_arrays.c
+++ asgn4/Problem B/growing_arrays.c
@@ -3,30 +3,29 @@
 #include <string.h>
 #include "growing_arrays.h"
 
-struct Table amisha_table;
 
 // returns the index of the item added  
 int add_record(Record new_record) {
-    if (amisha_table.nval == amisha_table.max) {
+    if (table.nval == table.max) {
         // The array should grow to two times the existing size
-        amisha_table.max *= GROW_SIZE;
-        Record *new_records = realloc(amisha_table.record, amisha_table.max * sizeof(Record));
+        table.max *= GROW_SIZE;
+        Record *new_records = realloc(table.record, table.max * sizeof(Record));
         if (!new_records) return -1;
-        amisha_table.record = new_records;
+        table.record = new_records;
     }
-    amisha_table.record[amisha_table.nval] = new_record;
-    amisha_table.nval++;
-    return amisha_table.nval - 1;
+    table.record[table.nval] = new_record;
+    table.nval++;
+    return table.nval - 1;
 }
 
 // return 0 if there is no matching record
 // return 1 if there is a matching record and deletes it and 
 // moves records
 int del_record(const char *name) {
-    for (int i = 0; i < amisha_table.nval; i++) {
-        if (strcmp(amisha_table.record[i].name, name) == 0) {
-            memmove(&amisha_table.record[i], &amisha_table.record[i + 1], (amisha_table.nval - i - 1) * sizeof(Record));
-            amisha_table.nval--;
+    for (int i = 0; i < table.nval; i++) {
+        if (strcmp(table.record[i].name, name) == 0) {
+            memmove(&table.record[i], &table.record[i + 1], (table.nval - i - 1) * sizeof(Record));
+            table.nval--;
             return 1;
         }
     }
@@ -35,9 +34,9 @@ int del_record(const char *name) {
 
 // returns id of student record of the first matching record
 int get_id(const char *name) {
-    for (int i = 0; i < amisha_table.nval; i++) {
-        if (strcmp(amisha_table.record[i].name, name) == 0) {
-	    return amisha_table.record[i].id;
+    for (int i = 0; i < table.nval; i++) {
+        if (strcmp(table.record[i].name, name) == 0) {
+	    return table.record[i].id;
         }
     }
     return 0;
@@ -45,8 +44,8 @@ int get_id(const char *name) {
 
 // returns record with a valid index, 0 to max-1 
 Record get_record(int index) {
-    if (index >= 0 && index < amisha_table.nval) {
-        return amisha_table.record[index];
+    if (index >= 0 && index < table.nval) {
+        return table.record[index];
     }
     // If the index is invalid, return a default Record
 Record empty_record = {0};
@@ -57,9 +56,9 @@ Record empty_record = {0};
 // Testing code. You can modify this and check your own test cases.
 /*
 int main() {
-    amisha_table.nval = 0;
-    amisha_table.max = INIT_SIZE;
-    amisha_table.record = malloc(sizeof(Record) * amisha_table.max);
+    table.nval = 0;
+    table.max = INIT_SIZE;
+    table.record = malloc(sizeof(Record) * table.max);
 
     // test add_record
     Record r1 = {"Alice", 1};
